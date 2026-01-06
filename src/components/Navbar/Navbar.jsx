a49e906169d01cc7d2bb576f9350ce809bbe2fd2
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-06 19:46:24 +0530

    Icons Added , Styles Changes Basic UI fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,37 @@
+import { IoSearchSharp } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
 
 const Navbar = ({ movie, setMovie, handleSearch, favourite }) => {
-
-    const showFavourite = () => {
-        console.log(favourite)
-    }
-    return (
-        <div>
-            <nav>
-                <div className="logo">
-                    <h1>Dekho  Movie</h1>
-                </div>
-
-                <div className="search">
-                    <form action="" onSubmit={(e) => handleSearch(movie, e)}>
-                        <input type="text" placeholder='Search movie' value={movie} onChange={(e) => setMovie(e.target.value)} />
-                        <button className="btn" type='submit'>Search</button>
-                    </form>
-                </div>
-
-                <div className='fav'>
-                    <button className="btn" onClick={showFavourite} >Fav</button>
-                </div>
-            </nav>
+  const showFavourite = () => {
+    console.log(favourite);
+  };
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <h1>Dekho Movie</h1>
         </div>
-    )
-}
 
-export default Navbar
+        <div className="search">
+          <form action="" onSubmit={(e) => handleSearch(movie, e)}>
+            <input
+              type="text"
+              placeholder="Search movie"
+              value={movie}
+              onChange={(e) => setMovie(e.target.value)}
+            />
+            <div className="search-btn">
+              <IoSearchSharp />
+            </div>
+          </form>
+        </div>
+
+        <div className="fav">
+          <FaRegHeart />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
